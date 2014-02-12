e02a0fcb0bef9a544e2aad5d231c8bf38ae1a3e4
Simeon Vincent <svincent@gmail.com>
2014-02-11 22:19:17 -0800

    Can you smell the freshly generated express app?

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
+
+/**
+ * Module dependencies.
+ */
+
 var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
 var app = express();
-var port = 3000;
 
-// app.get('/hello.txt', function(req, res){
-// 	var body = 'Hello, world!';
-// 	res.setHeader('Content-Type', 'text/plain');
-// 	res.setHeader('Content-Length', Buffer.byteLength(body));
-// 	res.end(body);
-// });
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-var logReq = function(req) {
-	console.log(Date.now() + ": " + req.ip + " " + req.method + " \"" + req.originalUrl + "\"");
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
 }
 
-app.get('/hello.text', function(req, res){
-	logReq(req);
-	res.send('Hello World!');
-});
+app.get('/', routes.index);
+app.get('/users', user.list);
 
-app.listen(port);
-console.log('Listening on port ' + port);
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
